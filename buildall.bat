335ec1bdf5df7b6f5aae5039e2226e1294c58f48
BlueWhaleMain <bluewhalemain@outlook.com>
2021-01-17 18:59:02 +0800

    重新整理项目

---
+++ buildall.bat
@@ -0,0 +1,5 @@
+@rem Copyright by BlueWhale. All Rights Reserved.
+@echo off
+call build.bat Library 2.4.0.2-1.16.1
+call build.bat LibRes 1.6.0.2-1.16.1
+call build.bat LibRes 1.6.0.2-1.16.1-ÎÞGUI°æ±¾ rd /s /q Pack\LibRes\assets\minecraft\textures\gui\.
---
+++ buildres.bat
@@ -0,0 +1,4 @@
+@rem Copyright by BlueWhale. All Rights Reserved.
+@echo off
+call build.bat LibRes 1.6.0.2-1.16.1
+call build.bat LibRes 1.6.0.2-1.16.1-ÎÞGUI°æ±¾ rd /s /q Pack\LibRes\assets\minecraft\textures\gui\.
